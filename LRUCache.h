614f0d769e5f4094b5bd471419b4efeb3b019b5a
wjcgithub <wjc163@sina.cn>
2017-02-22 18:09:48 +0800

    添加lRU缓存接口

---
+++ LRUCache.h
@@ -0,0 +1,18 @@
+/*
+* LRU 缓存接口
+*/
+#ifndef LRUCACHE_H_INCLUDED
+#define LRUCACHE_H_INCLUDED
+
+//创建L缓存
+int LRUCacheCreate(int capacity, void **lruCache);
+//销毁LRU缓存
+int LRUCacheDestory(void *lruCache);
+//将数据放入LRU缓存中
+int LRUCacheSet(void *lruCache, char key, char data);
+//从缓存中获取数据
+int LRUCacheGet(void *lruCache, char key);
+//打印缓存中的数据,按照访问时间从新到旧的顺序输出
+int LRUCachePrint(void *lruCache);
+
+#endif // LRUCACHE_H_INCLUDED
